11e4bb280482fc324eddd81ee6a24a32be6c3573
Jeffero277 <40421715+Jeffero277@users.noreply.github.com>
2019-06-08 15:42:19 +0200

    Add files via upload

---
+++ source_files_extraction_from_sln/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CHasz3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFolder = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // chooseFolder
+            // 
+            this.chooseFolder.Location = new System.Drawing.Point(12, 24);
+            this.chooseFolder.Name = "chooseFolder";
+            this.chooseFolder.Size = new System.Drawing.Size(108, 29);
+            this.chooseFolder.TabIndex = 0;
+            this.chooseFolder.TabStop = false;
+            this.chooseFolder.Text = "Choose folder";
+            this.chooseFolder.UseVisualStyleBackColor = true;
+            this.chooseFolder.Click += new System.EventHandler(this.chooseFolder_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(12, 59);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(799, 447);
+            this.logBox.TabIndex = 1;
+            this.logBox.Text = "";
+            this.logBox.TextChanged += new System.EventHandler(this.logBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 518);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.chooseFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseFolder;
+        private System.Windows.Forms.RichTextBox logBox;
+    }
+}
+
---
+++ source_files_extraction_from_sln/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace CHasz3
+{
+    public partial class Form1 : Form
+    {
+        private string slnPath;
+        FolderBrowserDialog Fdb;
+        public Form1()
+        {
+            slnPath = "";
+            Fdb = new FolderBrowserDialog();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string findSln(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.sln");
+
+            logBox.Refresh();
+            if (files.Length== 0)
+            {
+                logBox.Text += "SLN NOT FOUND\n\n";
+                return "";
+            }
+            logBox.Text += "SLN FOUND\n";
+            logBox.Text += files[0] + "\n";
+            return files[0];
+        }
+
+        private string findProj(string path)
+        {
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("Project"))
+                {
+                    break;
+                }
+            }
+            int start = line.IndexOf(",") + 3;
+            int end = line.IndexOf(".csproj") + 7;
+            line = line.Substring(start, end-start);
+            return line;
+        }
+
+        private List<string> findProjFiles(string path)
+        {
+            List<string> files = new List<string>();
+            XmlDocument projDoc = new XmlDocument();
+            projDoc.PreserveWhitespace = true;
+            projDoc.Load(path);
+
+            List<XmlNodeList> nodes = new List<XmlNodeList>();
+            nodes.Add(projDoc.GetElementsByTagName("Compile"));
+            nodes.Add(projDoc.GetElementsByTagName("EmbeddedResource"));
+            nodes.Add(projDoc.GetElementsByTagName("None"));
+
+            logBox.Text += "Project files: \n";
+            foreach (XmlNodeList node in nodes)
+            {
+                foreach (XmlNode item in node)
+                {
+                    logBox.Text +=slnPath + "\\" + item.Attributes["Include"].Value + "\n";
+                    files.Add(slnPath + "\\" + item.Attributes["Include"].Value);
+                }
+            }
+            return files;
+        }
+      
+        private void KowalskiAnalysis()
+        {
+            string csprojFile = "";
+            string slnFile = findSln(slnPath);
+            if (slnFile != "") {
+               csprojFile = findProj(slnFile);
+            };
+            csprojFile = String.Concat(slnPath, "\\", csprojFile);
+            logBox.Text += csprojFile + "\n\n";
+            slnPath = slnFile.Substring(0, slnFile.Length - 4);
+            List<string> Files = findProjFiles(csprojFile);
+            copyProjFiles(Files);
+        }
+
+        static string ConvertStringArrayToString(string[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append('\\');
+            }
+
+            return builder.ToString().Substring(0, builder.ToString().Length-1);
+        }
+
+        private void copyProjFiles(List<string> files)
+        {
+            string[] parentPaths;
+            string parentPath;
+            string temp;
+            string copyDir = slnPath + "\\" + "Copy";
+            Directory.CreateDirectory(copyDir);
+            foreach (string file in files)
+            {
+                parentPaths = file.Split(Path.DirectorySeparatorChar);
+                temp = ConvertStringArrayToString(parentPaths);
+                parentPath =temp.Substring(0, temp.Length - parentPaths[parentPaths.Length-1].Length -1);
+                if (!Directory.Exists(parentPath.Replace(slnPath, copyDir)))
+                {
+                    Directory.CreateDirectory(parentPath.Replace(slnPath, copyDir));
+                }
+                File.Copy(file, file.Replace(slnPath, copyDir));
+            }
+            logBox.Text += "\n\n COPY DIR: " + copyDir + "\n SUCCESS";
+
+        }
+
+        private void chooseFolder_Click(object sender, EventArgs e)
+        {
+            Fdb.ShowDialog();
+            slnPath = Fdb.SelectedPath;
+            logBox.Text += "Selected path: ";
+            logBox.Text += slnPath;
+            logBox.Text += "\n\n";
+            KowalskiAnalysis();
+        }
+
+        private void logBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
